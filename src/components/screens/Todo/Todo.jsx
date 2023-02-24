7941fbbcf40970b45a33bc0df05f158211e91041
ww_side <daniil.metelya@gmail.com>
2023-02-24 17:03:36 +0200

    add 'Todo' component

--- src/components/screens/Todo/Todo.jsx
+++ src/components/screens/Todo/Todo.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import TodoItem from './Item/TodoItem';
+import classes from './Todo.module.scss'
+
+const todos = [
+  {
+    id: 0,
+    title: 'Send message at 5 am',
+    isCompleted: false
+  },
+  {
+    id: 1,
+    title: 'Read the new book',
+    isCompleted: false
+  },
+  {
+    id: 2,
+    title: 'English',
+    isCompleted: false
+  }
+];
 
 const Todo = () => {
   return (
-    <div>
-      Todo
+    <div className={classes.Todo}>
+      <div>
+        <h1>ToDo List</h1>
+        {todos.map(todo => (
+          <TodoItem key={todo.id} todo={todo}/>
+        ))}
+      </div>
     </div>
   );
 };
