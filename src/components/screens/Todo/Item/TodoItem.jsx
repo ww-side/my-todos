668208859efc2600686a9ea60e05201a0bc21799
ww_side <daniil.metelya@gmail.com>
2023-02-24 20:28:41 +0200

    formed task card with funcional

--- src/components/screens/Todo/Item/TodoItem.jsx
+++ src/components/screens/Todo/Item/TodoItem.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Check from '../../../UI/Check/Check';
 import classes from './TodoItem.module.scss'
+import RemoveButton from '../../../UI/RemoveButton/RemoveButton';
 
-const TodoItem = ({todo}) => {
+const TodoItem = ({todo, changeTodo, removeTodo}) => {
   return (
-    <div className={classes.TodoItem}>
-      <Check/>
-      {todo.title}
+    <div className={classes.todoItem}>
+      <div className={classes.todoItemWrapper} onClick={() => changeTodo(todo.id)}>
+      <div><Check isCompleted={todo.isCompleted}/></div>
+      <span className={todo.isCompleted ? classes.todoTextCompleted : classes.todoText}>
+        {todo.title}
+      </span>
+      </div>
+      <RemoveButton todo={todo} removeTodo={removeTodo}/>
     </div>
   );
 };
