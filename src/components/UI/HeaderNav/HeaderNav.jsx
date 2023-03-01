ef0e391aabc6519d65193a57f3debb335fd4f681
ww_side <daniil.metelia@gmail.com>
2023-03-01 12:41:28 +0200

    added header

---
+++ src/components/UI/HeaderNav/HeaderNav.jsx
@@ -0,0 +1,32 @@
+import classes from './HeaderNav.module.scss';
+import {Link} from 'react-router-dom';
+import logo from '../../../assets/notepadLogo.png';
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
+import {useState} from 'react';
+
+const HeaderNav = () => {
+  const [nav, setNav] = useState(false);
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <div>
+          <Link to='/home'><img className={classes.logoItem} src={logo} alt='logo'/></Link>
+        </div>
+        <ul className={nav ? [classes.menu, classes.active].join(' ') : [classes.menu]}>
+          <li>
+            <Link className={classes.links} to='/home' onClick={() => setNav(!nav)}>Home</Link>
+          </li>
+          <li>
+            <Link className={classes.links} to='/todo' onClick={() => setNav(!nav)}>Todo</Link>
+          </li>
+        </ul>
+        <div onClick={() => setNav(!nav)} className={classes.mobileBtn}>
+          {nav ? <AiOutlineClose size={50}/> : <AiOutlineMenu size={50}/>}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderNav;
