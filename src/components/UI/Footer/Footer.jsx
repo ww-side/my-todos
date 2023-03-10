b454956284357378da3980881bede69648e4867f
ww_side <daniil.metelia@gmail.com>
2023-03-10 16:43:45 +0200

    fixed github icon in footer

--- src/components/UI/Footer/Footer.jsx
+++ src/components/UI/Footer/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import classes from './Footer.module.scss';
-import githubIcon from '../../../assets/githubIcon.png';
+import githubIcon from '../../../assets/githubLogo.png';
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <a href='https://github.com/ww-side' target='_blank'><img src={githubIcon} alt="github"/></a>
+      <a href='https://github.com/ww-side'><img src={githubIcon} alt="github"/></a>
     </footer>
   );
 };
